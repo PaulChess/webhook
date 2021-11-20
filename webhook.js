431a95a584127598959a5a9fc519b49d775362c3
paulChess <sjq15251852316@163.com>
2021-11-21 00:44:27 +0800

    edi

--- webhook.js
+++ webhook.js
@@ -3,6 +3,16 @@ let http = require('http');
 const server = http.createServer(function(req, res) {
   console.log(req.method, req.url);
   if (req.method == 'POST' && req.method == '/webhook') {
+    let buffers = [];
+    req.on('data', function(buffer) {
+      buffers.push(buffer);
+    });
+    req.on('end', function() {
+      let body = Buffer.concat(buffers);
+      let event = req.headers['x-gitHub-event']; // event = push
+      // github请求来的时候，要传递请求体body, 另外还会传一个signature过来，需要验证签名对不对
+      let sig = req.headers['x-hub-signature'];
+    })
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ ok: true }));
   } else {
