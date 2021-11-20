0a75bf6a26cdd82668544bc64dcfa218918f04fc
paulChess <sjq15251852316@163.com>
2021-11-21 00:57:40 +0800

    add vue-back.sh

--- webhook.js
+++ webhook.js
@@ -1,4 +1,11 @@
 let http = require('http');
+let crypto = require('crypto');
+
+const SECRET = '123456';
+
+function sign(body) {
+  return `sha1=${crypto.createHmac('sha1', SECRET).update(body).digest('hex')}`;
+}
 
 const server = http.createServer(function(req, res) {
   console.log(req.method, req.url);
@@ -12,6 +19,9 @@ const server = http.createServer(function(req, res) {
       let event = req.headers['x-gitHub-event']; // event = push
       // github请求来的时候，要传递请求体body, 另外还会传一个signature过来，需要验证签名对不对
       let sig = req.headers['x-hub-signature'];
+      if (sig !== sign[body]) {
+        return res.end('Not Allowed');
+      }
     })
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ ok: true }));
